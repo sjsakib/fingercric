1bdd87fd68e9a3cb4f88c6039ac1e09589ac455b
S.j. Sakib <sjsakib.bd@gmail.com>
2018-02-21 20:06:20 +0600

    added basic style and modal

--- client/main.js
+++ client/main.js
@@ -7,6 +7,9 @@ Vue.component('playerbar', {
             </span>\
             '
 });
+Vue.component('modal', {
+  template: '#modal-template',
+});
 
 var serverUrl = window.fingercric.serverUrl;
 
@@ -24,6 +27,7 @@ function vmCreated() {
       vm.p1.batting = false;
       vm.p2.batting = true;
     }
+    vm.waiting = false;
   });
 
   socket.on('move', function(n){
@@ -40,7 +44,8 @@ function move(n, player) {
   else p2.moved = n;
 
   if (p1.moved && p2.moved) {
-    this.last = p1.moved + " <----> " + p2.moved;
+    this.last.p1 = p1.moved;
+    this.last.p2 = p2.moved;
 
     if (p1.moved !== p2.moved) {
       if (p1.batting) p1.score += p1.moved;
@@ -53,7 +58,7 @@ function move(n, player) {
       p2.moved = 0;
     }
     if (p2.moved === p1.moved) {
-      this.last += " (out!)";
+      this.last.status += "out!";
 
       // select winner
       if (!this.chasing) this.chasing = true;
@@ -91,6 +96,8 @@ var app = new Vue({
   el: '#app',
   data: {
     roomId: getRoomId(),
+    waiting: true,
+    href: window.location.href,
     p1: {
       name: 'You',
       moved: 0,
@@ -104,7 +111,11 @@ var app = new Vue({
       batting: false,
     },
     chasing: false,
-    last: '',
+    last: {
+      p1: '...',
+      p2: '...',
+      status: ''
+    },
   },
   methods: {
     move: move,
